dce9414e060222cc5064ddf0d7a33247d584b716
Yusuf Efe Azaklı <77747662+ysfeeazkl@users.noreply.github.com>
2022-07-16 22:34:19 +0300

    ChessTable Prefab

---
+++ Assets/Scripts/Entity/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Entity
+{
+    public class Game
+    {
+        public bool order { get; set; }
+        public List<object> GameObjectList { get; set; }
+        public List<object> ChessTableSquare { get; set; } 
+    }
+}
---
+++ Assets/Scripts/Entity/Piece.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Entity
+{
+    public class Piece
+    {
+        public List<object> DangerSquare { get; set; }
+        public object MainSquare { get; set; }
+        public string PieceType { get; set; }
+    }
+}
